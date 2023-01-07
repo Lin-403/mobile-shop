a52fa609da17c6cf6b43cd0c8a304865f24b760a
Lin <1537053400@qq.com>
2023-01-07 20:18:35 +0800

    base main

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+function Product(props) {
+    const {product}=props
+    console.log(product._id)
+  return (
+    <Card className='my-3 py-3 rounded'>
+        <a href={`/products/${product._id}`}>
+            <Card.Img src={product.image} variant="top" />
+        </a>
+        <Card.Body>
+            <a href={`products/${product._id}`}>
+                <Card.Title>{product.name}</Card.Title>
+            </a>
+            <Card.Text as="div">
+            <div className='my-3'>
+                {product.rating}  是依据{product.numReviews}条评价数来计算 
+            </div></Card.Text>
+            <Card.Text as="h3">￥{product.price}</Card.Text>
+        </Card.Body>
+    </Card>
+  )
+}
+
+export default Product
---
+++ frontend/src/views/HomeView.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Row,Col} from "react-bootstrap"
+import Product from '../components/Product'
+import products from "../products"
+
+function HomeView() {
+  return (
+    <>
+      <h1>最新产品</h1>
+      <Row>{products.map(product=>(
+        <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product}/>
+        </Col>
+      ))}</Row>
+    </>
+  )
+}
+
+export default HomeView
